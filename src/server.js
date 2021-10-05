4087ceba77cec902a862b155fe2f8b3ead9c7039
singhabhishek6 <sabhisheksingh6@gmail.com>
2021-10-05 14:33:58 +0530

    cooki

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const hasTag = require('./controllers/hashtag.controller');
 //express midleware
 
 const app = express();
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: false, credentials: true }));
 app.use(express.json());
 //Passport configs
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
